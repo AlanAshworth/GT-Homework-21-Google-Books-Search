9b28f7d255c022a669fe0ea431ce7437556b3ca2
Alan Ashworth <awashworth927@gmail.com>
2020-05-08 12:08:30 -0400

    Modify displayed characters for 'Google Books'. Created shared Jumbotron component.

---
+++ client/src/components/shared/Jumbotron.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import "../../App.css";
+
+const Jumbotron = () => {
+  return (
+    <Fragment>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">(React) Google Books Search</h1>
+          <p className="lead">Search for and Save Books of Interest</p>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Jumbotron;
