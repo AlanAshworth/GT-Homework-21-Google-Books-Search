61d0fcc684d49b66ea97a9627729443897bdcfb8
Alan Ashworth <awashworth927@gmail.com>
2020-05-08 07:42:04 -0400

    Develop base create-react-app for client, add server with dependencies, add resources for project goals.

---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Test for running server
+app.get("/api/config", (req, res) => {
+    res.json({
+      success: true,
+      currentPort: PORT,
+    });
+  });
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API Server listening on http://localhost:${PORT}`);
+});
