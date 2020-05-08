e4bd80edab29fe623ad5b9cf53914f8406f3a981
Alan Ashworth <awashworth927@gmail.com>
2020-05-08 10:41:12 -0400

    Navbar added to React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/shared/Navbar"; 
 import Search from "./containers/Search";
 import Saved from "./containers/Saved";
 import './App.css';
@@ -7,6 +8,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Route exact path="/" component={Search}/>
       <Route exact path="/search" component={Search}/>
       <Route exact path="/saved" component={Saved}/>
