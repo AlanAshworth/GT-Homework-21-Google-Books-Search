063a6fe7e54af4636a8e18275a4c92258f43f99a
Alan Ashworth <awashworth927@gmail.com>
2020-05-08 10:04:52 -0400

    Create blank components necessary for project. Routed in App.

---
+++ client/src/containers/Search.jsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class Search extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>Search Page</h1>
+            </Fragment>
+        );
+    }
+}
+
+export default Search;
